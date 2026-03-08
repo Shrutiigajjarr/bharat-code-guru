0a70232dedb5f702242dc7fcb88cfab954ebf8d5
Shruti Gajjar <gajjarrshrutii@gmail.com>
2026-03-08 14:17:27 +0000

    Show continue learning on detail

--- src/components/CourseCard.tsx
+++ src/components/CourseCard.tsx
@@ -33,9 +33,6 @@ export function CourseCard({ track, onClick }: CourseCardProps) {
           <div className="progress-bar-animated h-full" style={{ width: `${track.progress}%` }} />
         </div>
       </div>
-      <Button onClick={onClick} size="sm" className="w-full mt-4">
-        {t('Continue Learning', 'सीखना जारी रखें')} →
-      </Button>
     </div>
   );
 }
